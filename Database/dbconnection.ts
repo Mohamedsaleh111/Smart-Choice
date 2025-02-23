c001d44170902126941f77fc3cc4e78122ca1a17
Mohamedsaleh111 <167624092+Mohamedsaleh111@users.noreply.github.com>
2025-02-24 00:31:30 +0200

    Update dbconnectioon.ts

--- Database/dbconnection.ts
+++ Database/dbconnection.ts
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 
 export async function dbconnection  (){
     await connect(process.env.DATABASE_DB as string).then(()=>{
-        console.log('db connected successfully');
+        console.log('db connectedddd successfully');
         
     }).catch(()=>{
         console.log('failed to connected to db');
